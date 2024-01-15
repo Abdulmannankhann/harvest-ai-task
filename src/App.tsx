beaefba59079cabfe824829a13682c78b3ca532e
Abdul9.khan <Abdul9.khan@ril.com>
2024-01-16 01:27:32 +0530

    Change barChart route url

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import Header from "./components/Header";
 import BarChartComponent from "./pages/BarChart";
-import Weather from "./pages/WeatherForcast";
+import WeatherForcast from "./pages/WeatherForcast";
 import "./App.css";
 import Home from "./pages/Home";
 
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/dynamic-bar-chart" element={<BarChartComponent />}></Route>
-          <Route path="/weather-forecast-line-graph" element={<Weather />}></Route>
+          <Route path="/weather-forecast-line-graph" element={<WeatherForcast />}></Route>
           <Route path="*" element={<div>Error 404</div>}></Route>
         </Routes>
       </CssBaseline>
